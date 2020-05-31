bcca0097415d137268d90186ea03f8f3c33c473f
Brian Crafton <crafton.b@husky.neu.edu>
2020-05-31 17:04:12 -0400

    setup script, fixing path refs, gitignore

--- sort/sort.py
+++ sort/sort.py
@@ -276,9 +276,10 @@ if __name__ == '__main__':
   total_frames = 0
   colours = np.random.rand(32, 3) #used only for display
   if(display):
-    if not os.path.exists('mot_benchmark'):
-      print('\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
+    if not os.path.exists('../2DMOT2015'):
+      print('Path not found!')
       exit()
+      
     plt.ion()
     fig = plt.figure()
     ax1 = fig.add_subplot(111, aspect='equal')
